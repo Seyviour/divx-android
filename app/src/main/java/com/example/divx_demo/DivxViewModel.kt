36ff1dab51be535318fe4044786b664669917e3a
seyviour <owolabisaviour@gmail.com>
2023-04-28 15:55:53 +0100

    Add connection listener to viewmodel

--- app/src/main/java/com/example/divx_demo/DivxViewModel.kt
+++ app/src/main/java/com/example/divx_demo/DivxViewModel.kt
@@ -1,28 +1,40 @@
 package com.example.divx_demo
 
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.media3.common.Player
 import androidx.media3.datasource.RawResourceDataSource
+import com.example.divx_demo.ble.ConnectionEventListener
+import com.example.divx_demo.ble.ConnectionManager
+import com.example.divx_demo.ble.toHexString
+import com.example.divx_demo.ble.toXCode
+import java.util.*
+import kotlin.collections.HashMap
 
 val _hardCodedResources: HashMap<String, XResourceType> = hashMapOf(
+    "ABCDBCDA" to XVideo("ABCDBCDA", RawResourceDataSource.buildRawResourceUri(R.raw.newton)),
     "ABCDBCD" to XQuiz("ABCD"),
     "ABCDBCDBDA" to XQuiz("ABCDBCDBDA"),
     "ABCDBCDCDA" to XQuiz("ABCDBCDCDA"),
-    "ABCDBCABDA" to XQuiz("ABCDBCABDA"),
-    "0x00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E" to XVideo("0x00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E", RawResourceDataSource.buildRawResourceUri(R.raw.newton))
+    "ABCDBCABDA" to XQuiz("ABCDBCABDA")
 )
 
+const val TAG_VIEW_MODEL = "VIEWMODEL"
 
 class DivxViewModel: ViewModel() {
+
+    val X_CODE_CHAR_UUID = "0000ff01-0000-1000-8000-00805f9b34fb"
+    val DIVX_SERVICE_UUID = "000000ff-0000-1000-8000-00805f9b34fb"
 
     private val isConnected = MutableLiveData<Boolean>()
     val connectionState: LiveData<Boolean> get() = isConnected
 
     fun setConnectionState(connected: Boolean) {
-        isConnected.value = connected
+        isConnected.postValue(connected)
     }
 
     fun getHardMappedResource(xid: String): XResourceType?{
@@ -31,19 +43,89 @@ class DivxViewModel: ViewModel() {
         return null
     }
 
+    fun charToXCode(characteristic: String?): String{
+        characteristic?.let{
+            val firstPeriod = it.indexOf(".")
+            if (firstPeriod > 0)
+                return characteristic.substring(0,firstPeriod)
+        }
+        return ""
+    }
+
+
+    private val xCodeNotificationsEnabledMLD = MutableLiveData<Boolean>(false)
+    val xCodeNotificationsEnabled: LiveData<Boolean> get() = xCodeNotificationsEnabledMLD
+
+    fun setXCodeNotificationState(state: Boolean){
+        xCodeNotificationsEnabledMLD.postValue(state)
+    }
+
+    var vmGatt:BluetoothGatt? = null
+
+    var currXCodeMLD: MutableLiveData<String> = MutableLiveData<String>("")
+    val currXCode: LiveData<String> get() = currXCodeMLD
+    fun setCurrXCode(xCode: String){ currXCodeMLD.postValue(xCode)}
+
     var player: Player? = null;
 
     val hardCodedResources: HashMap<String, XResourceType> = _hardCodedResources
 
     var bluetoothDevice: MutableLiveData<BluetoothDevice> = MutableLiveData<BluetoothDevice>()
-    val device : LiveData<BluetoothDevice> get() = bluetoothDevice
+    val vmDevice : LiveData<BluetoothDevice> get() = bluetoothDevice
 
     fun setBluetoothDevice(device: BluetoothDevice){
-        bluetoothDevice.value = device
+        bluetoothDevice.postValue(device)
     }
 
     companion object{
         const val X_CODE_CHAR_UUID = "0000ff01-0000-1000-8000-00805f9b34fb"
         const val DIVX_SERVICE_UUID = "000000ff-0000-1000-8000-00805f9b34fb"
     }
+
+    var vwConnectionListener = ConnectionEventListener().apply{
+
+        onCharacteristicChanged = {device, characteristic ->
+            Log.d(TAG_VIEW_MODEL, "onCharacteristicChanged")
+            if (characteristic.uuid == UUID.fromString(X_CODE_CHAR_UUID) && device==vmDevice.value){
+                setCurrXCode(characteristic.value.toXCode())
+            }
+        }
+
+        onConnectionSetupComplete = {gatt ->
+            vmGatt = gatt
+            setConnectionState(true)
+            bluetoothDevice.value?.let{it_device->
+                var characteristic = ConnectionManager.getCharacteristic(it_device, UUID.fromString(X_CODE_CHAR_UUID))
+                characteristic?.let{it_characteristic->
+                    ConnectionManager.enableNotifications(it_device, it_characteristic)
+                }
+            }
+        }
+
+        onDisconnect = {device ->
+            if (device == vmDevice.value)
+                setConnectionState(false)
+        }
+
+        onNotificationsEnabled = {device, characteristic ->
+            if (device == vmDevice.value && characteristic.uuid.toString() == X_CODE_CHAR_UUID)
+                setXCodeNotificationState(true)
+        }
+
+        onNotificationsDisabled = {device, characteristic ->
+            if (device == vmDevice.value && characteristic.uuid.toString() == X_CODE_CHAR_UUID)
+                setXCodeNotificationState(false)
+        }
+
+    }
+
+    fun registerListener(){
+        ConnectionManager.registerListener(vwConnectionListener)
+    }
+
+    fun unRegisterListener(){
+        ConnectionManager.registerListener(vwConnectionListener)
+    }
+
+
 }
